1213d3ea97e4e1ce4b7e427640cbd8b6550b62aa
andy <a.chaa@marketinvoice.com>
2016-08-10 17:51:54 +0100

    firebase

--- src/App.js
+++ src/App.js
@@ -2,33 +2,7 @@ import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import AppointmentForm from './AppointmentForm';
 
-var onSubmit = (values, dispatch) => {
-
-};
-
-class App extends Component {
-  render() {
-
-    return (
-      <div>
-        <AppointmentForm {...this.props} />
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    appointmentDate: state.appointmentDate
-  };
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-  }
-}
 
 
-App = connect(mapStateToProps, mapDispatchToProps) (App);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,76 @@
-import React from 'react';
+import React, { PropTypes, Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import { connect, Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
 import createLogger from 'redux-logger'
 import { reducer as formReducer } from 'redux-form';
-import App from './App';
+import AppointmentForm from './AppointmentForm';
 import './index.css';
 
-var reducers = {
+var rootReducer = combineReducers({
   form: formReducer
-};
-
-var rootReducer = combineReducers(reducers);
+});
 
 const loggerMiddleware = createLogger();
 
-var configureStore = function configureStore(initialState) {
-    const store = createStore(rootReducer, initialState, compose(
-        applyMiddleware(
-            loggerMiddleware
-        ),
-        window.devToolsExtension ? window.devToolsExtension() : f => f
-    ));
-    return store;
-};
+let store = createStore(
+  rootReducer,
+  {},
+  compose(applyMiddleware(loggerMiddleware), window.devToolsExtension ? window.devToolsExtension() : f => f)
+)
 
+const mapStateToProps = (state) => {
+  return {
+    appointmentDate: state.appointmentDate
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    test: () => console.log('test')
+  }
+}
+
+
+class App extends Component {
+  onAuthStateChanged() {
+    console.log('test');
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(this.onAuthStateChanged);
+  }
+
+  handleClick() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+
+  render() {
+    return (
+      <div>
+        <AppointmentForm {...this.props} />
+        <section id="page-splash">
+          <h3 className="logo">Database Web Quickstart</h3>
+          <div>
+            <button onClick={this.handleClick} id="sign-in-button" className="btn btn-default">
+              <i className="glyphicon glyphicon-play"></i> Sign in with Google
+            </button>
+            <button onClick={e=>test()}>Test</button>
+          </div>
+        </section>
+
+      </div>
+    );
+  }
+}
+
+const ConnectedRootComponent = connect(mapStateToProps, mapDispatchToProps)(App)
+
+// App = connect(mapStateToProps, mapDispatchToProps) (App);
 
 ReactDOM.render(
-  <Provider store={configureStore()}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
