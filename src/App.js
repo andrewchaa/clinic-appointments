060c65b7080ae29e92b355a8ec516047ba0403db
Hye-Eun Hills <Hyeeun@Hye-Euns-Air.lan>
2016-08-11 22:59:58 +0100

    firebase auth change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
 import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import AppointmentForm from './AppointmentForm';
+import SplashPage from './SplashPage';
 
+class App extends Component {
 
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(this.props.onAuthStateChanged);
+  }
 
+  render() {
+
+    return (
+      <div>
+        <AppointmentForm {...this.props} />
+        <SplashPage {...this.props} />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    userId: state.userId
+  };
+}
+
+const mapDispatchToProps = function(dispatch) {
+  return {
+    handleClick() {
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider);
+    },
+    onAuthStateChanged: (user) => {
+      console.log(user);
+      dispatch({
+        type: 'User_Id_Changed',
+        userId: user.uid
+      });
+    },
+    testFunction: function() {
+      console.log('test');
+    }
+  }
+}
+
+App = connect(mapStateToProps, mapDispatchToProps)(App)
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { connect, Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
 import createLogger from 'redux-logger'
 import { reducer as formReducer } from 'redux-form';
-import AppointmentForm from './AppointmentForm';
+import App from './App';
 import './index.css';
 
 var rootReducer = combineReducers({
@@ -19,55 +19,6 @@ let store = createStore(
   compose(applyMiddleware(loggerMiddleware), window.devToolsExtension ? window.devToolsExtension() : f => f)
 )
 
-const mapStateToProps = (state) => {
-  return {
-    appointmentDate: state.appointmentDate
-  };
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    test: () => console.log('test')
-  }
-}
-
-
-class App extends Component {
-  onAuthStateChanged() {
-    console.log('test');
-  }
-
-  componentDidMount() {
-    firebase.auth().onAuthStateChanged(this.onAuthStateChanged);
-  }
-
-  handleClick() {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
-  }
-
-  render() {
-    return (
-      <div>
-        <AppointmentForm {...this.props} />
-        <section id="page-splash">
-          <h3 className="logo">Database Web Quickstart</h3>
-          <div>
-            <button onClick={this.handleClick} id="sign-in-button" className="btn btn-default">
-              <i className="glyphicon glyphicon-play"></i> Sign in with Google
-            </button>
-            <button onClick={e=>test()}>Test</button>
-          </div>
-        </section>
-
-      </div>
-    );
-  }
-}
-
-const ConnectedRootComponent = connect(mapStateToProps, mapDispatchToProps)(App)
-
-// App = connect(mapStateToProps, mapDispatchToProps) (App);
 
 ReactDOM.render(
   <Provider store={store}>
