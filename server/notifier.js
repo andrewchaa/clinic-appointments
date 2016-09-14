2c07ca3228e04f4b8f6deec14f914d4413cfed0b
andy <a.chaa@marketinvoice.com>
2016-09-14 14:07:34 +0100

    notification text change

--- server/notifier.js
+++ server/notifier.js
@@ -10,13 +10,31 @@ exports.send = (appointment) => {
 
   client.sendMessage({
     to: appointment.mobile,
-    from: '+441274451343',
-    body: 'Hi ' + appointment.name  + '\r\n' +
-      'You have an appointment with Hye-Eun at ' + appointment.clinic +
-      ' tomorrow, ' + appointment.date + ' ' + appointment.hour + ':' + appointment.minute + '\r\n' +
-      'If you cannot make it, please call Hye-Eun and rearrange it.'
+    from: process.env.twilioFromNumber,
+    body: `Hi ${appointment.name}\r\n` +
+          `This is a reminder for your acupuncture appointment with Hye-Eun ` +
+          `at ${appointment.clinic} tomorrow, ` +
+          `${appointment.date} ${appointment.hour}:${appointment.minute}\r\n` +
+          `If you cannot make it, please let Hye-Eun know ASAP to rearrange it. ` +
+          `We look forward to seeing you then.`
   }, function (err, responseData){
-    console.log(err);
+    if (err) {
+      console.log(err);
+    }
+
+    if (responseData) {
+      console.log(responseData);
+    }
   });
+  // client.sendMessage({
+  //   to: appointment.mobile,
+  //   from: '+441274451343',
+  //   body: 'Hi ' + appointment.name  + '\r\n' +
+  //     'You have an appointment with Hye-Eun at ' + appointment.clinic +
+  //     ' tomorrow, ' + appointment.date + ' ' + appointment.hour + ':' + appointment.minute + '\r\n' +
+  //     'If you cannot make it, please call Hye-Eun and rearrange it.'
+  // }, function (err, responseData){
+  //   console.log(err);
+  // });
 
 }
