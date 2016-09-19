a6ce21e445c23bf3bf180f57d0ed538a22803d50
Andy <andrew.chaa@yahoo.co.uk>
2016-09-19 18:09:07 +0100

    added bootstrap

--- renameBuild.js
+++ renameBuild.js
@@ -1,15 +1,17 @@
 const rimraf = require('rimraf');
 const fs = require('fs');
+const fse = require('fs-extra');
 
 rimraf('app', function (error) {
-  if (error) {
-    console.log(error);
-  }
+  if (error) console.log(error);
 
   fs.rename('build', 'app', function (err) {
-    if (err) {
-      console.log(err);
-    }
+    if (err) console.log(err);
+
+    fse.copy('bootstrap', 'app/bootstrap', function (err) {
+      if (err) console.log(err);
+    })
+
   })
 
 });
